a599997f53158f71581b9eb215541bf97554b3ed
Lewis Kirkaldie <lewis@cinegy.com>
2020-08-26 14:12:14 +0100

    -add arm var

--- main.tf
+++ main.tf
@@ -2,12 +2,13 @@
 // Modules
 module "cinegy_base" {
   source  = "app.terraform.io/cinegy/cinegy-base/aws"
-  version = "0.0.8"
+  version = "0.0.9"
 
   app_name = "air-test"
   aws_region = "eu-west-1"
   customer_tag = "CINEGY"
   environment_name = "dev"
+  aws_secrets_privatekey_arn = "arn:aws:secretsmanager:eu-west-1:564731076164:secret:terraform-cinegycentral-deployment/dev/privatekey.pem-GVW7XA"
   domain_name = var.domain_name
   domain_admin_password = var.domain_admin_password
 }
