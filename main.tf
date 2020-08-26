d7efea8cf9bca50761c66108b62faece84a5106e
Lewis Kirkaldie <lewis@cinegy.com>
2020-08-26 15:07:18 +0100

    -split vm to module

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "cinegy_base" {
 
   app_name = "air-test"
   aws_region = "eu-west-1"
-  customer_tag = "CINEGY"
+  customer_tag = "IABM"
   environment_name = "dev"
   aws_secrets_privatekey_arn = "arn:aws:secretsmanager:eu-west-1:564731076164:secret:terraform-cinegycentral-deployment/dev/privatekey.pem-GVW7XA"
   domain_name = var.domain_name
@@ -15,5 +15,14 @@ module "cinegy_base" {
 
 module "cinegy-base-winvm" {
   source  = "app.terraform.io/cinegy/cinegy-base-winvm/aws"
-  version = "0.0.1"
+  version = "0.0.5"
+
+  app_name = "air-test"
+  aws_region = "eu-west-1"
+  customer_tag = "IABM"
+  environment_name = "dev"
+
+  instance_profile_name = module.cinegy_base.instance_profile_default_ec2_instance_name
+  vpc_id = module.cinegy_base.main_vpc
+  directory_service_default_doc_name = module.cinegy_base.directory_service_default_doc_name
 }
