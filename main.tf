234773b4a1c86b2d7ba14d210367ec20473c2d13
Lewis Kirkaldie <lewis@cinegy.com>
2020-08-26 11:56:57 +0100

    -add domain

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@
 // Modules
 module "cinegy_base" {
   source  = "app.terraform.io/cinegy/cinegy-base/aws"
-  version = "0.0.3"
+  version = "0.0.5"
 
   app_name = "air-test"
   aws_region = "eu-west-1"
   customer_tag = "CINEGY"
   environment_name = "dev"
+  domain_admin_password = var.domain_admin_password
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "domain_admin_password" {
+    description = "Domain admin password - sensitive value, recommended to be passed in via environment variables"
+    type        = string
+}
