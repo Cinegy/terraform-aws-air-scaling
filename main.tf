ea66945a592fd007931597028af7eea6f362e11e
Lewis Kirkaldie <lewis@cinegy.com>
2020-12-11 08:33:43 +0000

    -update for later module -launch AMD instance

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "cinegy"
 
     workspaces {
-      name = "terraform-aws-air-scaling"
+      name = "terraform-aws-air-testing"
     }
   }
 }
@@ -13,10 +13,11 @@ terraform {
 locals {
   app_name = "air-scaletest"
   aws_region = "eu-west-1"
-  customer_tag = "IABM"
+  customer_tag = "cinegy"
   environment_name = "demo"
   directory_type = "SimpleAD"
   directory_edition = null
+  route53_zone_name = "qa.cinegy.net"
 }
 
 # define the specific providers, including providers required to pass into modules
@@ -35,8 +36,9 @@ provider "template" {
 
 # install the base infrastructure required to support other module elements
 module "cinegy_base" {
+  
   source  = "app.terraform.io/cinegy/cinegy-base/aws"
-  version = "0.0.12"
+  version = "0.0.13"
 
   app_name = local.app_name
   aws_region = local.aws_region
@@ -50,52 +52,10 @@ module "cinegy_base" {
   directory_edition = local.directory_edition
 }
 
-# create a sysadmin machine for RDP access
-module "sysadmin-vm" {
-  source  = "app.terraform.io/cinegy/cinegy-base-winvm/aws"
-  version = "0.0.20"
-
-  app_name          = local.app_name
-  aws_region        = local.aws_region
-  customer_tag      = local.customer_tag
-  environment_name  = local.environment_name  
-  instance_profile  = module.cinegy_base.instance_profile_default_ec2_instance_name
-  vpc_id            = module.cinegy_base.main_vpc
-  ad_join_doc_name  = module.cinegy_base.ad_join_doc_name
-  join_ad           = true  
-
-  ami_name          = "Windows_Server-2019-English-Full-Base*"
-  host_name_prefix  = "SYSADMIN1A"
-  host_description  = "${upper(local.environment_name)}-Sysadmin Terminal (SYSADMIN) 1A"
-  instance_subnet   = module.cinegy_base.public_subnets.a
-  instance_type     = "t3.medium"
-
-  security_groups = [
-    module.cinegy_base.remote_access_security_group,
-    module.cinegy_base.remote_access_udp_6000_6100,
-    module.cinegy_base.open_internal_access_security_group
-  ]
-
-  user_data_script_extension = <<EOF
-  Uninstall-WindowsFeature -Name Windows-Defender
-  Set-Service wuauserv -StartupType Disabled
-
-  Install-WindowsFeature -Name RSAT-DNS-Server,RSAT
-
-  Install-CinegyPowershellModules
-  Install-DefaultPackages
-  Install-Product -PackageName Cinegy-License-Service-Trunk -VersionTag dev
-  Get-AwsLicense -UseTaggedHostname $true
-  Set-LicenseServerSettings -AllowSharing $true -ServiceUrl "https://api.central.cinegy.com/awsmrkt/v1/license/renew?serialId="
-
-  RenameHost
-EOF
-}
-
 # create VMs to run Air workloads
 module "cinegy-air" {
   source  = "app.terraform.io/cinegy/cinegy-base-winvm/aws"
-  version = "0.0.20"
+  version = "0.0.28"
 
   count = var.air_vm_count
 
@@ -106,57 +66,65 @@ module "cinegy-air" {
   instance_profile  = module.cinegy_base.instance_profile_default_ec2_instance_name
   vpc_id            = module.cinegy_base.main_vpc  
   ad_join_doc_name  = module.cinegy_base.ad_join_doc_name
-  join_ad           = true 
+  join_ad           = true
+  #tenancy           = "host" 
 
   ami_name          = "Windows_Server-2019-English-Full-Base*"
-  host_name_prefix  = "AIR-${count.index+1}A"
+  host_name_prefix  = "AIR${count.index+1}A"
   host_description  = "${upper(local.environment_name)}-Playout (AIR) ${count.index+1}A"
-  instance_subnet   = module.cinegy_base.private_subnets.a
-  instance_type     = "g4dn.2xlarge"
+  instance_subnet   = module.cinegy_base.public_subnets.a
+  instance_type     = "g4ad.4xlarge"
+  create_external_dns_reference = true
+  route53_zone_name = local.route53_zone_name
+  attach_data_volume = true
+  data_volume_size = 100
 
   security_groups = [
     module.cinegy_base.remote_access_security_group,
     module.cinegy_base.remote_access_udp_6000_6100,
-    module.cinegy_base.open_internal_access_security_group
+    module.cinegy_base.remote_access_playout_engine_rest_5521_5600
   ]
 
   user_data_script_extension = <<EOF
   
-  Uninstall-WindowsFeature -Name Windows-Defender
-  Set-Service wuauserv -StartupType Disabled
-
   Install-CinegyPowershellModules
   Install-DefaultPackages
-  Install-Product -PackageName Cinegy-Air-Trunk -VersionTag stage
-  Install-Product -PackageName Thirdparty-AirNvidiaAwsDrivers-v14.x -VersionTag dev
-  Set-LicenseServerSettings -RemoteLicenseAddress "SYSADMIN1A-${upper(local.environment_name)}"
+  Install-Product -PackageName Cinegy-License-Service-Trunk -VersionTag dev
+  Install-Product -PackageName Cinegy-Air-v15.x -VersionTag dev
+  Install-Product -PackageName Thirdparty-MetricBeat-NVGPU-v6.x -VersionTag dev
+  Install-Product -PackageName Thirdparty-AWSCLI-v2.x -VersionTag prod
   
+  Get-AwsLicense -UseTaggedHostname $true
+  Set-LicenseServerSettings -AllowSharing $true -ServiceUrl "https://api.central.cinegy.com/awsmrkt/v1/license/renew?serialId="
+  
+  #fix Air Control DLL registry failure
+  Start-Process -FilePath "C:\Program Files\Cinegy\Cinegy Air PRO (x64) 15.0.0\CinegyAirServerEx.exe" -ArgumentList "/regserver"
+
   #format scratch disk
   Get-Disk | Where-Object partitionstyle -eq 'raw' | 
   Initialize-Disk -PartitionStyle MBR -PassThru | 
   New-Partition -AssignDriveLetter -UseMaximumSize | 
   Format-Volume -FileSystem NTFS -NewFileSystemLabel "DATA" -Confirm:$false 
 
-  #download test content
-  $bucket = 'cinegyqa-simple-playout-content'
+  # #download test content
+  # $bucket = 'awscinegytesting-sample-content'
 
-  $files = Get-S3Object -BucketName $bucket -KeyPrefix 'scripts'
-  foreach($file in $files) {
-    Copy-S3Object -SourceBucket $bucket -SourceKey $($file.Key) -LocalFolder 'd:\'
-  }
+  # $files = Get-S3Object -BucketName $bucket -KeyPrefix 'scripts'
+  # foreach($file in $files) {
+  #   Copy-S3Object -SourceBucket $bucket -SourceKey $($file.Key) -LocalFolder 'd:\'
+  # }
 
-  $files = Get-S3Object -BucketName $bucket -KeyPrefix 'content'
-  foreach($file in $files) {
-    Copy-S3Object -SourceBucket $bucket -SourceKey $($file.Key) -LocalFolder 'd:\'
-  }
-
-  $multiviewerAddress = "MV" + ${count.index+1} + ".qa.terraform.cinegy.net"
-  [System.Environment]::SetEnvironmentVariable('MULTIVIEWER_ADDRESS', $multiviewerAddress, [System.EnvironmentVariableTarget]::Machine)
   [System.Environment]::SetEnvironmentVariable('ENGINE_COUNT', ${var.engine_count}, [System.EnvironmentVariableTarget]::Machine)
 	
-  $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:45
-  Register-ScheduledJob -Trigger $trigger -FilePath D:\scripts\run-test-cycle.ps1 -Name TestCycle
+  # $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:45
+  # Register-ScheduledJob -Trigger $trigger -FilePath D:\scripts\run-test-cycle.ps1 -Name TestCycle
+
+  Uninstall-WindowsFeature -Name Windows-Defender
+  Set-Service wuauserv -StartupType Disabled
 
   RenameHost
+
+  # #install NV drivers last, since they can trigger a reboot
+  # Install-Product -PackageName Thirdparty-AirNvidiaAwsDrivers-v14.x -VersionTag dev
 EOF
 }
